c955361bfb943d4b40317f07d9731ca57db28264
Anay Wadhera <anay1018@gmail.com>
2019-11-06 22:32:17 -0800

    support: refactor for androidx

--- Android.mk
+++ Android.mk
@@ -6,16 +6,15 @@ LOCAL_USE_AAPT2 := true
 LOCAL_MODULE := VendorSupportLib
 
 LOCAL_SHARED_ANDROID_LIBRARIES := \
-    android-support-annotations \
-    android-support-v4 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v14-preference
+    androidx.annotation_annotation \
+    androidx.core_core \
+    androidx.preference_preference \
+    androidx.appcompat_appcompat \
+    androidx.recyclerview_recyclerview \
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_JAR_EXCLUDE_FILES := none
+LOCAL_JAR_EXCLUDED_FILES := none
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
